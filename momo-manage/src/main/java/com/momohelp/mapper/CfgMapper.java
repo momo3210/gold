da0e4ed54a06a23e07db09235f6f2c471f9963ae
unknown <3203317@qq.com>
2016-05-11 11:42:39 +0800

    haha

---
+++ momo-manage/src/main/java/com/momohelp/mapper/CfgMapper.java
@@ -0,0 +1,13 @@
+package com.momohelp.mapper;
+
+import com.momohelp.model.Cfg;
+import com.momohelp.util.MyMapper;
+
+/**
+ *
+ * @author Administrator
+ *
+ */
+public interface CfgMapper extends MyMapper<Cfg> {
+
+}
